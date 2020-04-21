bb3279bac5dacc425a9afd21b0ce612d179ad0ec
andersonvalesan <andersonvalesan@andersonvalesan>
2020-04-21 18:47:28 -0300

    Part 2-FrontComponent

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
-require_once __DIR__.'/init.php';
 
-$name = $request->get('name', 'World');
+header("Location: http://localhost:8000/web");
 
-$response = new Response(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
-
-$response->send();
 ?>
