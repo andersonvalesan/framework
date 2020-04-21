94028f83d61cbaa08c2ea596cc5a2abab1d993e0
andersonvalesan <andersonvalesan@andersonvalesan>
2020-04-21 16:17:35 -0300

    creating new bye file and reuse code

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
-require_once __DIR__.'/vendor/autoload.php';
-
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
-$request = Request::createFromGlobals();
+require_once __DIR__.'/init.php';
 
 $name = $request->get('name', 'World');
 
