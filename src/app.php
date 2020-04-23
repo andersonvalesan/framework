5f2454bb20d7f0c523a7de8f5616fef7b76c81e0
andersonvalesan <andersonvalesan@andersonvalesan>
2020-04-23 14:26:31 -0300

    templating

--- src/app.php
+++ src/app.php
@@ -1,8 +1,49 @@
 <?php
 use Symfony\Component\Routing;
+use Symfony\Component\HttpFoundation\Response;
+
+function is_leap_year($year = null){
+    if (null === year) {
+         $year = date('y');
+    }
+  
+    return 0 === $year % 400 || (0 === $year %4 && 0 !== $year % 100);
+}
 
 $routes = new Routing\RouteCollection();
-$routes->add('hello', new Routing\Route('/hello/{name}', ['name' => 'World']));
-$routes->add('bye', new Routing\Route('/bye'));
+
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', [
+    'year' => null,
+    '_controller' => function ($request) {
+        if (is_leap_year($request->attributes->get('year'))){
+            return new Response('Sim, esse é um ano bissexto!');
+        }
+
+        return new Response('Não, esse não é um ano bissexto.');
+     }
+]));
+
+$routes->add('hello', new Routing\Route('/hello/{name}', [
+    'name' => 'World',
+    '_controller' => function ($request) {
+        $request->attributes->set('foo', 'bar');
+    
+        $response = render_template($request);
+
+        $response->headers->set('Content-Type', 'text/plain');
+
+        return $response;
+    }
+]));
+
+$routes->add('bye', new Routing\Route('/bye', [
+    '_controller' => function ($request) {
+        $response = render_template($request);
+
+        $response->headers->set('Content-Type', 'text/plain');
+
+        return $response;
+    } 
+]));
 
 return $routes;
--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,1 +1,2 @@
-Hello <?= htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?>
+Hello <?= htmlspecialchars($name, ENT_QUOTES, 'UTF-8')."\n" ?>
+Hello <?= htmlspecialchars($foo, ENT_QUOTES, 'UTF-8') ?>
